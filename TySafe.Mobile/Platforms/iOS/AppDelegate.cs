803a71239c2a9cbf2ca159a7518d0dd5de324b54
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-05-06 19:18:25 +0200

    Fix Projects

--- TySafe.Mobile/Platforms/iOS/AppDelegate.cs
+++ TySafe.Mobile/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,8 @@
 ﻿using Foundation;
 
 namespace TySafe.Mobile;
-
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
